9e7451da3333789fb4ca7906613cbc2e58934f31
Craig Ambrose <craig@craigambrose.com>
2010-06-15 22:26:29 +1200

    fixed admin scaffold

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,2 +1,12 @@
+# create_table "communities", :force => true do |t|
+#   t.string   "name"
+#   t.string   "url"
+#   t.text     "short_description"
+#   t.text     "long_description"
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+# end
+
 class Community < ActiveRecord::Base
+  validates_presence_of :name, :url, :short_description
 end
